e0d55bf3828fc1be7f790166745c125a578ed593
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-05-01 14:31:43 +0200

    made drag and drop functionality stable

--- static/js/drag_and_drop.js
+++ static/js/drag_and_drop.js
@@ -1,112 +1,86 @@
 export const drag_and_drop = {
     // cards: document.querySelectorAll("[draggable='true']"),
     // cols: document.querySelectorAll('.column-content'),
-    // currentCard: null,
-    // dragOverCard: null,
+
+    currentCard: null,
+    dragOverCard: null,
     cards: null,
     cols: null,
 
-    init: function () {
-        this.cards = null;
-        this.cols = null;
+
+    init: function() {
 
         this.cards = document.querySelectorAll("[draggable='true']");
         this.cols = document.querySelectorAll('.column-content');
-        // const cards = document.querySelectorAll("[draggable='true']");
-        // const cols = document.querySelectorAll('.column-content');
+        console.log(this.cards, "cards")
+        console.log(this.cols, "cards")
 
-       let currentCard = null;
-       let dragOverCard = null;
-        // this.refreshCardState();
-        // this.refreshColumState();
-        // console.log(this.cards);
-
-        for(let card of this.cards) {
-            card.addEventListener('dragstart', dragStart);
-            card.addEventListener('dragend', dragEnd);
-            card.addEventListener('dragenter', dragEnterCard)
-            card.addEventListener('dragleave', dragLeaveCard)
-        }
-
-        for(let col of this.cols) {
-            col.addEventListener('dragover', dragOver);
-            col.addEventListener('dragenter', dragEnter);
-            col.addEventListener('dragleave', dragLeave);
-            col.addEventListener('drop', dragDrop);
-        }
-
-        function dragEnterCard(e) {
-            e.preventDefault();
-            dragOverCard = e.target;
-            dragOverCard.classList.add('hovered');
-            // console.log('dragEnterCard: ', dragOverCard)
-        }
-
-        function dragLeaveCard(e) {
-            e.preventDefault();
-            if(dragOverCard != null) {
-                dragOverCard.classList.remove('hovered');
-                dragOverCard = null;
-            }
-            // console.log('dragLeaveCard: ', dragOverCard)
+        for (let card of drag_and_drop.cards) {
+            card.addEventListener('dragstart', drag_and_drop.dragStartCard);
+            card.addEventListener('dragend', drag_and_drop.dragEndCard);
+            card.addEventListener('dragenter', drag_and_drop.dragEnterCard)
+            card.addEventListener('dragleave', drag_and_drop.dragLeaveCard)
+            card.addEventListener('drop', drag_and_drop.dropCard);
 
         }
 
-        function dragOver(e) {
-            e.preventDefault();
+        for(let col of drag_and_drop.cols) {
+            col.addEventListener('dragover', drag_and_drop.dragOver);
+            col.addEventListener('dragenter', drag_and_drop.dragEnter);
+            col.addEventListener('dragleave', drag_and_drop.dragLeave);
+            col.addEventListener('drop', drag_and_drop.dragDrop);
         }
 
-        function dragEnter(e) {
-            e.preventDefault();
-            this.className = 'column-content hovered';
-        }
-
-        function dragLeave(e) {
-            this.className = 'column-content';
-        }
-
-        function dragDrop(evt) {
-            console.log('dragDrop ======')
-            console.log(currentCard);
-            this.className = 'column-content';
-
-            if(dragOverCard != null) {
-                // setCurrentCard(dragOverCard, currentCard);
-
-                dragOverCard.insertAdjacentElement('beforebegin', currentCard);
-                dragOverCard.classList.remove('hovered')
-                dragOverCard = null;
-            } else {
-                if(currentCard != null)
-                    this.append(currentCard)
-            }
-
-            // currentCard = null;
-        }
-
-        function dragStart(evt) {
-            currentCard = evt.target;
-            console.log(currentCard);
-
-            // this.className += ' hold';
-            this.classList.add('hold');
-            setTimeout(() => {
-                this.className = 'invisible';
-            }, 0);
-        }
-
-        function dragEnd(evt) {
-            this.className = 'card';
-            // dragOverCard.classList.remove('hovered')
-        }
-},
-
-    refreshCardState: function () {
-        this.cards = document.querySelectorAll("[draggable='true']");
-        // this.init();
     },
-    refreshColumState: function () {
-        this.cols = document.querySelectorAll('.column-content');
+
+    dragStartCard: function dragStart(evt) {
+        drag_and_drop.currentCard = evt.target;
+
+        evt.target.classList.add('hold');
+        setTimeout(() => {
+            this.className = 'invisible';
+        }, 0);
+    },
+
+    dragEndCard: function dragEnd(evt) {
+        evt.target.className = 'card';
+    },
+
+    dragEnterCard: function dragEnterCard(e) {
+        e.preventDefault();
+        drag_and_drop.dragOverCard = e.target;
+    },
+
+    dragLeaveCard: function dragLeaveCard(e) {
+        e.preventDefault();
+        if(drag_and_drop.dragOverCard != null) {
+            drag_and_drop.dragOverCard = null;
+        }
+
+    },
+    dragOver: function dragOver(e) {
+        e.preventDefault();
+    },
+
+    dragEnter: function dragEnter(e) {
+        e.preventDefault();
+    },
+
+    dragLeave: function dragLeave(e) {
+        console.log('column-content checker - e.target', e.target)
+    },
+
+    dragDrop: function (evt) {
+        if (!evt.target.className.includes('card')) {
+            console.log("DRAG AND DROP", evt.target);
+            evt.target.append(drag_and_drop.currentCard)
+        }
+    },
+    dropCard(evt) {
+        console.log("DRAG AND DROP CARD", evt.target);
+        drag_and_drop.dragOverCard.insertAdjacentElement('beforebegin', drag_and_drop.currentCard);
+        drag_and_drop.dragOverCard.classList.remove('hovered')
+        drag_and_drop.dragOverCard = null;
     }
 }
 
