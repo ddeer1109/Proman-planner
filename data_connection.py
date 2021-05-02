640a2605b1cda77467af18341dbbae804b10826a
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-05-02 20:28:06 +0200

    changed data_connection parameters for deploying env

--- data_connection.py
+++ data_connection.py
@@ -15,23 +15,19 @@ UPLOADED_IMAGES_FILE_PATH = pathlib.Path(f"{pathlib.Path(__file__).parent.absolu
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
-
-    env_variables_defined = user_name and password and host and database_name
-
-    if env_variables_defined:
+    # user_name = os.environ.get('PSQL_USER_NAME')
+    # password = os.environ.get('PSQL_PASSWORD')
+    # host = os.environ.get('PSQL_HOST')
+    # database_name = os.environ.get('PSQL_DB_NAME')
+    #
+    # env_variables_defined = user_name and password and host and database_name
+    #
+    # if env_variables_defined:
         # this string describes all info for psycopg2 to connect to the database
-        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            database_name=database_name
-        )
-    else:
-        raise KeyError('Some necessary environment variable(s) are not defined')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    return DATABASE_URL
+    # else:
+    #     raise KeyError('Some necessary environment variable(s) are not defined')
 
 
 def open_database():
